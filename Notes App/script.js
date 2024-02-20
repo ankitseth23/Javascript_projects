ad38656460682a1a9ccae39940953d1a1c55cf6e
ankitseth23 <157285789+ankitseth23@users.noreply.github.com>
2024-02-21 01:10:04 +0200

    Add files via upload

---
+++ Notes App/script.js
@@ -0,0 +1,46 @@
+let notesContainer = document.querySelector(".notes-container");
+let createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes(){
+    notesContainer.innerHTML= localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click",()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src ="Image/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+    updateStorage();
+});
+
+notesContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+
+    else if(e.target.tagName==="p"){
+        notes=document.querySelectorAll(".input-box");
+        notes.forEach(nt=>{
+            nt.onkey=function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown",event =>{
+    if (event.key ==="Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+        updateStorage();
+    }
+})
